2f2570a01941f5c7aa0f4a38463eb2f196e41d8e
Hadis Pishdast <51030506+hadis-pishdast@users.noreply.github.com>
2020-05-20 00:21:40 +0200

    Add files via upload

---
+++ LiveVideo.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+import time
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+cap = cv2.VideoCapture(0)
+time.sleep(2)
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = frame[y:y+h, x:x+w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(roi_color, (ex, ey), (ex+ew,ey+eh), (0,255,0),2)
+    cv2.imshow('frame', frame)
+    #cv2.imshow('gray', gray)
+    if cv2.waitKey(20) & 0xFF == ord('q'): 
+            break
+     
+cap.release()
+cv2.destroyAllwindows()
+ 
